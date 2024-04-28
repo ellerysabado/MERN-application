60ba9303cfe06b5eeecc78269780efb27eb5099a
Owen Jewell <ojewell@iastate.edu>
2024-04-27 20:10:59 -0500

    here

--- backend/index.js
+++ backend/index.js
@@ -15,18 +15,112 @@ const dbName = "secoms319";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
+  app.get("/products", async (req, res) => {
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
+    const query = {};
+    const results = await db
+        .collection("product")
+        .find(query)
+        .limit(100)
+        .toArray();
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
 
-app.get("/products", async (req, res) => {
-    try {
-      await client.connect();
-      const db = client.db(dbName);
-      const collection = db.collection("fakestore_catalog");
-      const products = await collection.find({}).toArray();
-      res.status(200).json(products);
-    } catch (error) {
-      console.error("Error retrieving products:", error);
+app.get("/:id", async (req, res) => {
+  try {
+      const productid = Number(req.params.id);
+      console.log("product to find: ", productid);
+      const query = {"id": productid };
+      const result = await db.collection("product").findOne(query);
+      if (!result){
+          res.status(404).send("Not Found");
+      }
+      else {
+          res.status(200).json(result);
+      }
+  }
+  catch (error) {
       res.status(500).send("Internal Server Error");
-    }
+  }
+});
+
+app.post("/addProducts", async (req, res) => {
+  try {
+      await client.connect();
+
+      if (!req.body || Object.keys(req.body).length === 0) {
+          return res.status(400).send({ error: 'Bad request: No data provided.' });
+      }
+
+      const values = Object.values(req.body);
+      const newDocument = {
+          "id": values[0],
+          "title": values[1],
+          "price": values[2],
+          "description": values[3],
+          "category": values[4],
+          "image": values[5],
+          "rating": values[6],
+      };
+
+      const existingDoc = await db.collection("product").findOne({ "id": newDocument.id });
+      if (existingDoc) {
+          return res.status(409).send({ error: 'Conflict: a product with this ID already exists' });
+      }
+
+      const results = await db.collection("product").insertOne(newDocument);
+      res.status(200).send(results);
+  } catch (error) {
+      console.error('Error adding new product:', error);
+      res.status(500).send({ error: 'An internal server error occurred' });
+  }
+});
+
+app.delete("/deleteproduct/:id", async (req, res) => {
+  try {
+      const id = Number(req.params.id);
+
+      await client.connect();
+      console.log("product to delete: ", id);
+
+      const query = {id: id};
+
+      //delete
+      const results = await db.collection("product").deleteOne(query);
+      res.status(200);
+      res.send(results);
+  }
+  catch (error) {
+      console.error("Error deleting product: ", error);
+      res.sendStatus(500).send({message: 'Internal Server Error'});
+  }
+});
+
+app.put("/updateproduct/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const query = { id: id };
+  await client.connect();
+  console.log("product to Update :",id);
+  // Data for updating the document, typically comes from the request body
+  console.log(req.body);
+  const updateData = {
+  $set:{
+  "title": req.body.title,
+  "price": req.body.price,
+  "description": req.body.description,
+  "category": req.body.category,
+  "image": req.body.image,
+  "rating": req.body.rating
+  }
+  };
+  // Add options if needed, for example { upsert: true } to create a document if it doesn't exist
+  const options = { };
+  const results = await db.collection("product").updateOne(query, updateData, options);
+  res.status(200);
+  res.send(results);
   });
 
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,168 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Author: Owen Jewell
+// ISU Netid: ojewell@iastate.edu
+// Date: April 15, 2024
+document.addEventListener('DOMContentLoaded', function () {
+  fetch("http://localhost:8081/listproducts")
+    .then(response => response.json())
+    .then(products => loadproducts(products))
+    .catch(error => {
+      console.error("Error loading products:", error)
+      displayError("Error loading products")
+    });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const newproductButton = document.getElementById('post-product');
+  newproductButton.addEventListener('click', addANewproduct);
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const fetchButton = document.getElementById('fetch-product');
+const productIdInput = document.getElementById('product-id-input');
+const movieContainer = document.getElementById('movie-container');
+
+fetchButton.addEventListener('click', function () {
+  const productId = productIdInput.value;
+  if (!productId) {
+    // If no ID provided, fetch all products
+    fetch("http://localhost:8081/listproducts")
+      .then(response => response.json())
+      .then(products => loadproducts(products))
+      .catch(error => {
+        console.error("Error loading products:", error)
+        displayError("Error loading products")
+      });
+  } else {
+    fetch(`http://localhost:8081/${productId}`)
+      .then(response => response.json())
+      .then(product => {
+        movieContainer.innerHTML = '';
+        if (!product.name) {
+          displayError("No product with that ID found");
+        }
+        else {
+          displayproduct(product);
+        }
+      })
+      .catch(error => {
+        console.error("Error loading products:", error);
+        displayError("Error loading products");
+      });
+  }
+});
+
+function loadproducts(products) {
+  movieContainer.innerHTML = '';
+  products.forEach(product => {
+    displayproduct(product);
+  })
+}
+
+function displayproduct(product) {
+  const cardDiv = document.createElement('div'); // Create a new div element for each card
+  cardDiv.classList.add('col');
+  cardDiv.innerHTML = `
+      <div class="card shadow-sm">
+          <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+          <div class="card-body">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text">${product.description}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Delete product</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `;
+  movieContainer.appendChild(cardDiv);
+}
+
+function displayError(message) {
+  const errorDiv = document.createElement('div');
+  errorDiv.classList.add('alert', 'alert-danger');
+  errorDiv.textContent = message;
+  movieContainer.appendChild(errorDiv);
+}
+
+function addANewproduct() {
+  const newproductInput = document.getElementById('new-product-input');
+  const newproductJson = newproductInput.value;
+
+  try {
+      const newproduct = JSON.parse(newproductJson);
+      console.log('New product JSON:', newproduct);
+
+      fetch("http://localhost:8081/addproduct", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newproduct)
+      })
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Failed to add new product.');
+          }
+          return response.json();
+      })
+      .then(data => {
+          console.log('New product added:', data);
+          // Optional: Refresh the list of products after adding
+          fetch("http://localhost:8081/listproducts")
+              .then(response => response.json())
+              .then(products => loadproducts(products))
+              .catch(error => {
+                  console.error("Error loading products:", error)
+                  displayError("Error loading products")
+              });
+      })
+      .catch(error => {
+          console.error('Error adding new product:', error);
+          displayError('Error adding new product.');
+      });
+
+      // Clear the input field after adding the product
+      newproductInput.value = '';
+  } catch (error) {
+      console.error('Invalid JSON for new product:', error);
+      displayError('Invalid JSON for new product.');
+  }
+}
+
+function updateOneproduct() {
+  // Fetch the value from the input field
+  let id = document.getElementById("updateproductById").value;
+  console.log(id);
+  fetch(`http://localhost:8081/updateproduct/${id}`, {
+    method: 'PUT',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(
+      {
+        "name": "product Abraham ALDACO-GASTELUM",
+        "price": 100.90,
+        "description": "I product is one example of an image for my exercise",
+        "imageUrl": "https://robohash.org/Abraham"
+      }
+    )
+  })
+    .then(response => response.json())
+    .then(updateThisproduct => { updateOneproductById(updateThisproduct) });
+
+}
+
+function deleteOneproduct() {
+  // Fetch the value from the input field
+  let id = document.getElementById("deleteproductById").value;
+  console.log(id);
+  fetch(`http://localhost:8081/deleteproduct/${id}`, {
+  method: 'DELETE',
+  headers: { 'content-type': 'application/json' },
+  body: JSON.stringify(
+  { "id":id}
+  )
+  })
+  .then(response => response.json())
+  .then(deleteThisproduct => {deleteOneproductById(deleteThisproduct)});
+}
+
+//{"id": 4,"name": "product Owen","price":50.4,"description": "I product is one example of an image for my exercise","imageUrl": "https://robohash.org/Owen"}
+
