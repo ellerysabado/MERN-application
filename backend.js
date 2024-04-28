b07ba5df78ba871feaa8a87ba3b8c8f5cae4f885
ellery <ellerysabado@gmail.com>
2024-04-27 19:29:10 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -14,3 +14,22 @@ const url = "mongodb://127.0.0.1:27017";
 const dbName = "secoms319";
 const client = new MongoClient(url);
 const db = client.db(dbName);
+
+
+app.get("/products", async (req, res) => {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection("fakestore_catalog");
+      const products = await collection.find({}).toArray();
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Error retrieving products:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+
+  app.listen(port, () => {
+    console.log(`Server listening at http://${host}:${port}`);
+  });
